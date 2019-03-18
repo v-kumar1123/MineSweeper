bd46a73e6a0665df86c57173a653bbe8312e814e
v-kumar1123 <varun.thegreate.kumar@gmail.com>
2019-03-18 09:11:42 -0500

    Init Commit 75

--- src/MinesweeperFrame.java
+++ src/MinesweeperFrame.java
@@ -1,9 +1,44 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
 import java.util.*;
 
 
 public class MinesweeperFrame extends JFrame{
+
+    BufferedImage dead;
+    BufferedImage oh;
+    BufferedImage down;
+    BufferedImage happy;
+    BufferedImage happyDown;
+    BufferedImage shades;
+    BufferedImage digitNine;
+    BufferedImage digitEight;
+    BufferedImage digitSeven;
+    BufferedImage digitSix;
+    BufferedImage digitFive;
+    BufferedImage digitFour;
+    BufferedImage digitThree;
+    BufferedImage digitHyphen;
+    BufferedImage digitTwo;
+    BufferedImage digitOne;
+    BufferedImage digitZero;
+    BufferedImage eight;
+    BufferedImage seven;
+    BufferedImage six;
+    BufferedImage five;
+    BufferedImage four;
+    BufferedImage three;
+    BufferedImage two;
+    BufferedImage one;
+    BufferedImage empty;
+    BufferedImage unclicked;
+    BufferedImage flag;
+    BufferedImage question;
+    BufferedImage mine;
+    BufferedImage incorrectFlag;
+    BufferedImage exploded;
+
 
     public MinesweeperFrame() {
         super("k0904676, Period 2");
--- src/MinesweeperGame.java
+++ src/MinesweeperGame.java
@@ -1,11 +1,26 @@
 public class MinesweeperGame {
     Tile[][] board;
+    int difficulty=0;
+
+    public static final int EASY=0;
+    public static final int MEDIUM=1;
+    public static final int HARD=2;
+
     public MinesweeperGame() {
-        board=new Tile[10][10];
         init();
     }
 
     public void init() {
+        if(difficulty==0) {
+            board=new Tile[10][10];
+        }
+        else if(difficulty==1) {
+            board=new Tile[15][15];
+        }
+
+        else if(difficulty==2) {
+            board=new Tile[20][20];
+        }
     }
 }
 
